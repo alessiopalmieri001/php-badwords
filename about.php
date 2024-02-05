fb61a39e9fb35d16df0c984bb7e1e148aa5b5346
alessiopalmieri001 <alessiopalmieri001@gmail.com>
2024-02-05 17:06:35 +0100

    codiviso paragrafo con about

---
+++ about.php
@@ -0,0 +1,23 @@
+<?php 
+
+$paragrafo = $_GET['paragrafo']
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <p>
+            <?php echo  $paragrafo   ?>
+        </p>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -7,7 +7,26 @@
     </head>
     <body>
         <h1>
-            
+            ciao pippo
         </h1>
+        <div>
+            <form action="./about.php" method="GET">
+                <div>
+                    <textarea name="paragrafo" id="" cols="30" rows="10">
+                            
+                    </textarea>
+                </div>
+                <div>
+                    <input type="text" name="censure" value="lorem">
+                </div>
+                <div>
+                    <button>
+                        clicca qui...
+                    </button>
+                </div>
+            </form>
+        </div>
+
+        
     </body>
 </html>
